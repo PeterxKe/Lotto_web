93d3aaed7bf2decfe421d21c47ea90e2ccb02fca
PeterxKe <kemmeterpeter@gmail.com>
2026-06-06 15:26:54 +0200

    Update lotto_web.py

--- lotto_web.py
+++ lotto_web.py
@@ -24,7 +24,7 @@ if wahl == "Über diese APP":
 
 else:
 
-    st.title("🎰 Lotto 6 aus 49")
+    st.title("-🎰 LLotto6 aus 49-")
     
     # Profit speichern
     if "profit" not in st.session_state:
